fc429208d3a0a00dee6ab61b935c0bb66999bce7
sqrtthree <imsqrtthree@gmail.com>
2018-08-12 21:54:24 +0800

    chore: Add some comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = (input) => {
+/**
+ * Convert input to an anchor link with github flavored markdown style.
+ * @param  {string} input title that needs to be converted
+ * @return {string}       anchor link
+ */
+module.exports = input => {
   return input.replace(/\s+/g, '-').toLowerCase()
 }
