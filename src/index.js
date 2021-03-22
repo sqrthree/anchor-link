3f51cfc047285dfc447ebf8c3a4d9e3d94ee5c85
sqrtthree <imsqrtthree@gmail.com>
2021-03-23 00:13:48 +0800

    style: Format

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
  * @param  {string} input title that needs to be converted
  * @return {string}       anchor link
  */
-module.exports = input => {
-  if (typeof input !== 'string') throw new Error('Expecting to receive a string but got a ' + typeof input)
+module.exports = (input) => {
+  if (typeof input !== 'string') {
+    throw new Error('Expecting to receive a string but got a ' + typeof input)
+  }
 
   return input
     .trim()
